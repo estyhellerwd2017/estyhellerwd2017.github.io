76521aacfc2ea3e68b8e2413f4c8ee9d267dfc41
estyhellerwd2017 <estyheller.cs@gmail.com>
2017-02-02 12:45:38 +0200

    change

---
+++ classes/class12/portfolio_page.php
@@ -0,0 +1,13 @@
+<? include("header.html"); ?>
+        <main class="container-fluid">
+            <div class="row main_row">
+                <div class="col-md-8 col-xs-6">
+                    <div>here is the large one</div>
+                 </div>
+                <div class="col-md-4 col-xs-6">
+                    <div>here is the small one</div>
+                </div>
+            </div>
+        </main>
+
+<? include("footer.html"); ?>
